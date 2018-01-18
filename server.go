d193f59ed87d0140ea91840f8ac0d59f157909f4
ISpoonJelly <tiitomoha@gmail.com>
2018-01-18 14:00:59 +0200

    added mgo with simple example

--- server.go
+++ server.go
@@ -1,19 +1,56 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"log"
+	"net/http"
 
-import "net/http"
+	"github.com/gin-gonic/gin"
+	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+)
 
-var port = ":8080"
+type Person struct {
+	Name  string
+	Phone string
+}
+
+var collPeople *mgo.Collection
 
 func main() {
+	session, err := mgo.Dial("localhost")
+	if err != nil {
+		panic(err)
+	}
+	defer session.Close()
+
+	collPeople = session.DB("test").C("people")
+	err = collPeople.Insert(&Person{"Tarek", "+201111984394"},
+		&Person{"3amooor", "+201223340190"})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	router := gin.Default()
 
 	router.GET("/status", getStatus)
+	router.GET("/people", getPeople)
 
-	router.Run(port)
+	router.Run(":8080")
 }
 
 func getStatus(c *gin.Context) {
 	c.String(http.StatusOK, "Server is up!")
+}
+
+func getPeople(cntx *gin.Context) {
+	var result []Person
+	err := collPeople.Find(bson.M{}).All(&result)
+
+	if err != nil {
+		cntx.JSON(http.StatusInternalServerError, bson.M{"error": err})
+		return
+	}
+
+	cntx.JSON(http.StatusOK, bson.M{"people": result})
 }
